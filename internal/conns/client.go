92969f5c61f9da31e61967a54e7c52eb7f43656b
Brad Sickles <brad.sickles@gmail.com>
2024-09-14 15:24:47 -0400

    Added distribution_invalidation

--- internal/conns/client.go
+++ internal/conns/client.go
@@ -1,7 +1,14 @@
 package conns
 
-import "github.com/aws/aws-sdk-go-v2/aws"
+import (
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudfront"
+)
 
 type Client struct {
 	Config aws.Config
+}
+
+func (c *Client) Cloudfront() *cloudfront.Client {
+	return cloudfront.NewFromConfig(c.Config)
 }
