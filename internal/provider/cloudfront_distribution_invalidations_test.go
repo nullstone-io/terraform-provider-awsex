5712d4fdb0ee357548b4847223dfaeccf25cc45e
Brad Sickles <brad.sickles@gmail.com>
2024-10-01 13:03:07 -0400

    fix tests

--- internal/provider/cloudfront_distribution_invalidations_test.go
+++ internal/provider/cloudfront_distribution_invalidations_test.go
@@ -1,10 +1,8 @@
 package provider
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/terraform"
 	"testing"
 )
 
@@ -13,8 +11,8 @@ func TestAccDistributionInvalidations(t *testing.T) {
 		t.Skip("skipping long-running test in short mode")
 	}
 
-	cdn1Id := testAccCreateCdn(t)
-	cdn2Id := testAccCreateCdn(t)
+	cdn1Id := testAccCreateCdn(t, "test1", "www.example1.com")
+	cdn2Id := testAccCreateCdn(t, "test2", "www.example2.com")
 	config1 := providerConfig + fmt.Sprintf(`
 resource "awsex_cloudfront_distribution_invalidations" "test" {
   distribution_ids = [%[1]q, %[2]q]
@@ -30,43 +28,11 @@ resource "awsex_cloudfront_distribution_invalidations" "test" {
 				Config: config1,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrSet("awsex_cloudfront_distribution_invalidations.test", "id"),
-					testAccCheckResourceMapAttribute("awsex_cloudfront_distribution_invalidations.test", "statuses", map[string]string{
-						cdn1Id: "Completed",
-						cdn2Id: "Completed",
-					}),
+					resource.TestCheckResourceAttr("awsex_cloudfront_distribution_invalidations.test", "statuses.%", "2"),
+					resource.TestCheckResourceAttr("awsex_cloudfront_distribution_invalidations.test", fmt.Sprintf("statuses.%s", cdn1Id), "Completed"),
+					resource.TestCheckResourceAttr("awsex_cloudfront_distribution_invalidations.test", fmt.Sprintf("statuses.%s", cdn2Id), "Completed"),
 				),
 			},
 		},
 	})
-}
-
-func testAccCheckResourceMapAttribute(resourceName, attributeName string, expected map[string]string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[resourceName]
-		if !ok {
-			return fmt.Errorf("resource not found: %s", resourceName)
-		}
-
-		// Retrieve the actual map attribute from the state
-		attr, ok := rs.Primary.Attributes[attributeName]
-		if !ok {
-			return fmt.Errorf("attribute not found: %s", attributeName)
-		}
-
-		// Parse the string-encoded map (assuming the attribute is stored as a JSON string or similar)
-		var actualMap map[string]string
-		err := json.Unmarshal([]byte(attr), &actualMap)
-		if err != nil {
-			return fmt.Errorf("failed to parse attribute %s: %s", attributeName, err)
-		}
-
-		// Compare the expected map with the actual map
-		for k, v := range expected {
-			if actualMap[k] != v {
-				return fmt.Errorf("expected key %s to have value %s, got %s", k, v, actualMap[k])
-			}
-		}
-
-		return nil
-	}
 }
